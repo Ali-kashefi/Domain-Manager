1f2902f3f0038e5e02a58e2e92760e78fbf6a55a
Ali Kashefi <alikashefi274@gmail.com>
2025-10-01 15:33:15 +0330

    feat:Adding styles specific to each state and changing the output from a string to a <p> tag

--- domain-manager/src/utils/Domain_status.js
+++ domain-manager/src/utils/Domain_status.js
@@ -1,13 +1,14 @@
-export default function Domain_status( state ) {
+export default function Domain_status(state) {
 
-    switch (state) { 
-        case 1: 
-            return "pending";
-        case 2: 
-            return "verified";
+    switch (state) {
+        case 1:
+            return (<p  className="text-amber-500 bg-amber-300 text-center rounded-3xl text-sm">pending</p>)
+
+        case 2:
+            return (<p className="text-green-500 bg-green-300 text-center rounded-3xl text-sm">verified</p>);
         case 3:
-            return "rejected"; 
+            return (<p className="text-red-500 bg-red-300 text-center rounded-3xl text-sm">rejected</p>);
 
-      
+
     }
 }
